89a2a59f8a9d05d786b971aed16e5d62a81290ad
aliarabian <aliarabian.dev@gmail.com>
2023-01-15 09:46:10 +0330

    initial commit

---
+++ src/main/java/com/tosan/learning/docker/firstdockerchallenge/FirstDockerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tosan.learning.docker.firstdockerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDockerChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDockerChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tosan/learning/docker/firstdockerchallenge/FirstDockerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosan.learning.docker.firstdockerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDockerChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
