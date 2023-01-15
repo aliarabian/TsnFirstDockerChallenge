5250a08101258de61145e7443ff69b69cad1210a
aliarabian <aliarabian.dev@gmail.com>
2023-01-15 16:08:30 +0330

    [BUGFIX] fixes printing null in case of having no query param

--- src/main/java/com/tosan/learning/docker/firstdockerchallenge/filters/MDCFilter.java
+++ src/main/java/com/tosan/learning/docker/firstdockerchallenge/filters/MDCFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,11 +14,12 @@ import java.io.IOException;
 @Component
 public class MDCFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        MDC.put("request", request.getRequestURI() + "?" + request.getQueryString());
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        var fullRequestUrl = StringUtils.isNoneEmpty(req.getQueryString()) ? req.getRequestURI() + "?" + req.getQueryString() : req.getRequestURI();
+        MDC.put("request", fullRequestUrl);
         MDC.put("status", "200");
         try {
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(req, res);
         } finally {
             MDC.clear();
         }
